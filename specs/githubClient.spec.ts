19fd27399f0f21b3eed3507181f3407cf4995d26
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-05-06 16:46:28 -0700

    tmp

--- specs/githubClient.spec.ts
+++ specs/githubClient.spec.ts
@@ -1,25 +1,25 @@
-import { MockCache } from "./mockCache";
+// import { assert, expect } from "chai";
 import { GithubObjectStorage } from "./../src/githubObjectStorage";
 import { XmlHttpRequestClient } from "@paperbits/common/http/xmlHttpRequestClient";
 import { GithubClient } from "./../src/githubClient";
-// import { assert, expect } from "chai";
 import { StaticSettingsProvider } from "./staticSettingsProvider";
-import { HttpClient } from "@paperbits/common/http";
 import * as atob from "atob";
+import { GithubBlobStorage } from "../src/githubBlobStorage";
+import * as Utils from "@paperbits/common";
+import { IGithubTreeItem } from "../src/IGithubTreeItem";
 
 
 describe("GithubClient", async () => {
-    it("Can create page metadata in specified locale when metadata doesn't exists.", async () => {
+    it("Can read and update data.", async () => {
         const settingsProvider = new StaticSettingsProvider({
             github: {
                 authorizationKey: "",
-                repositoryName: "",
-                repositoryOwner: ""
+                repositoryName: "test",
+                repositoryOwner: "azaslonov"
             }
         });
 
         global["atob"] = atob;
-        const localCache = new MockCache();
         const httpClient = new XmlHttpRequestClient();
         const githubClient = new GithubClient(settingsProvider, httpClient);
         const objectStorage = new GithubObjectStorage("src/data/demo.json", githubClient);
@@ -30,14 +30,55 @@ describe("GithubClient", async () => {
 
         await objectStorage.updateObject("pages/e0987ca1-f458-b546-7697-7be594b35583", result);
         await objectStorage.saveChanges();
+    });
 
-        // const result = await githubClient.getFileContent("src/data/demo.json");
-        console.log(result);
+    it("Can upload binary files.", async () => {
+        const settingsProvider = new StaticSettingsProvider({
+            github: {
+                authorizationKey: "",
+                repositoryName: "test",
+                repositoryOwner: "azaslonov"
+            }
+        });
 
-        // const result = await objectStorage.searchObjects("/");
+        global["atob"] = atob;
+        const httpClient = new XmlHttpRequestClient();
+        const githubClient = new GithubClient(settingsProvider, httpClient);
+        // const blobStorage = new GithubBlobStorage(githubClient);
 
-        // this.authorizationToken = githubSettings["authorizationKey"];
-        // this.repositoryName = githubSettings["repositoryName"];
-        // this.repositoryOwner = githubSettings["repositoryOwner"];
+        const content = Utils.stringToUnit8Array("Hello world");
+
+        const newTree = new Array<IGithubTreeItem>();
+
+
+        for (let i = 0; i < 2; i++) {
+            const filename = `bulk/fileAAA${i + 1}.txt`;
+
+            try {
+                const response = await githubClient.createBlob(filename, content);
+
+                const newTreeItem: IGithubTreeItem = {
+                    path: filename,
+                    sha: response.sha
+                };
+
+                newTree.push(newTreeItem);
+
+                console.log("OK");
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+
+        await githubClient.push("Test 1");
+
+        // const lastCommit = await githubClient.getLatestCommit();
+        // const tree = await githubClient.createTree(lastCommit.tree.sha, newTree);
+        // const message = `Updating website content.`;
+        // const newCommit = await githubClient.createCommit(lastCommit.sha, tree.sha, message);
+
+        // await githubClient.updateReference("master", newCommit.sha);
+        console.log("DONE");
     });
 });
